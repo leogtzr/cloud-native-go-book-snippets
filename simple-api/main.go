64c88764688b1b5cfb7c8e7c0a8e867a1fea3edf
leogtzr <leogutierrezramirez@gmail.com>
2021-04-24 10:17:42 -0600

    add delete endpoint

--- simple-api/main.go
+++ simple-api/main.go
@@ -37,8 +37,60 @@ func Put(key string, value string) error {
 	return nil
 }
 
+// keyValueGetHandler expects to be called with a GET request for
+// the "/v[1-9]/key/{key}" resource.
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+// keyValueDeleteHandler expects to be called with a DELETE request for
+// the "/v[1-9]/key/{key}" resource.
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	_, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Delete(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 // keyValuePutHandler expects to be called with a PUT request for
-// the "/v1/key/{key}" resource.
+// the "/v[1-9]/key/{key}" resource.
 func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
@@ -67,6 +119,8 @@ func main() {
 		curl -v -X PUT -d 'someValue' http://localhost:8080/v1/someKey
 	*/
 	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
