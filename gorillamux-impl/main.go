3c6095b8318e0e1f4f097e48c814b196821297a6
leogtzr <leogutierrezramirez@gmail.com>
2021-04-24 08:18:04 -0600

    add gorilla service impl

---
+++ gorillamux-impl/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func helloGoHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello net/http!\n"))
+}
+
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/", helloGoHandler)
+
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
