bfcb867c4c82eb9d8917e35610697757a20e2522
leogtzr <leogutierrezramirez@gmail.com>
2021-04-24 17:55:17 -0600

    add funding section

---
+++ simple-api-threadsafe/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	ErrorNoSuchKey = errors.New("no such key")
+	myMap          = struct {
+		sync.RWMutex
+		m map[string]string
+	}{
+		m: make(map[string]string),
+	}
+)
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+
+	return nil
+}
+
+func Put(key string, value string) error {
+	store[key] = value
+
+	return nil
+}
+
+// keyValueGetHandler expects to be called with a GET request for
+// the "/v[1-9]/key/{key}" resource.
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+// keyValueDeleteHandler expects to be called with a DELETE request for
+// the "/v[1-9]/key/{key}" resource.
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	_, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Delete(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// keyValuePutHandler expects to be called with a PUT request for
+// the "/v[1-9]/key/{key}" resource.
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	/*
+		curl -v -X PUT -d 'someValue' http://localhost:8080/v1/someKey
+	*/
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
